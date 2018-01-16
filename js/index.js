c6ff995c954bd83a0bcb8feed987709b89e5cd66
raulanatol <raul@natol.es>
2018-01-16 20:31:50 +0000

    Test js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,35 @@
-$(document).ready(function() {
-  $('.slider').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 2,
-      autoplay: true,
-      autoplaySpeed: 2000,
-  });
-  AOS.init();
-});
+var testimonios = [
+    { imagen: 'img/mobile-phon-2.png', text: 'bla bla bla bal' },
+    { imagen: 'img/mobile-phon-7.png', text: 'blube blueb belue' },
+    { imagen: 'img/mobile-phon-7.png', text: 'blube blueb belue' }
+];
 
-var elm = document.querySelector('#main-header');
-var ms = new MenuSpy(elm);
+
+function cargarTestimonios() {
+    for (var i = 0; i < testimonios.length; i++) {
+        var testimonio = '<div><img src="' + testimonios[i].imagen + '"><p>' + testimonios[i].text + '</p></div>';
+        $('#contenedor-testimonio').append(testimonio);
+    }
+}
+
+$(document).ready(function () {
+    cargarTestimonios();
+
+
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('#contenedor-testimonio').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+    AOS.init();
+});
